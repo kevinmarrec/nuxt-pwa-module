9ba58be3c76087105a88212417d1543e3325f8ac
Kevin Marrec <kevin@marrec.io>
2022-05-19 17:47:01 +0200

    chore: add hash to manifest file

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,4 +1,5 @@
 import { join } from 'pathe'
+import hasha from 'hasha'
 import { addTemplate, useNuxt } from '@nuxt/kit'
 import { PWAContext } from './types'
 
@@ -6,13 +7,18 @@ export default (pwa: PWAContext) => {
   if (!pwa.manifest) { return }
 
   const nuxt = useNuxt()
-  const filename = 'pwa.manifest.json'
+
+  const manifestJson = JSON.stringify(pwa.manifest, null, 2)
+
+  const filename = nuxt.options.dev
+    ? 'pwa.manifest.json'
+    : `pwa.manifest.${hasha(manifestJson, { algorithm: 'md5' }).slice(0, 8)}.json`
 
   addTemplate({
     filename,
     dst: join(pwa._assetsDir, filename),
     write: true,
-    getContents: () => JSON.stringify(pwa.manifest, null, 2)
+    getContents: () => manifestJson
   })
 
   pwa._manifestMeta = {
