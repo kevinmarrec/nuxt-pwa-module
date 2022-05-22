a73c881be6d2e49ec58b3b3f8e6ddc28ab0ccf75
xlanex6 <alexduval71@gmail.com>
2022-05-22 23:37:22 +0200

    feat(): Inject in Meta

--- src/meta.ts
+++ src/meta.ts
@@ -37,7 +37,23 @@ export default (pwa: PWAContext) => {
       head.link.push({ rel: 'apple-touch-icon', href: iconBig.src, sizes: iconBig.sizes })
     }
 
-    // TODO: Launch Screen Image (IOS)
+  }
+  // TODO: Launch Screen Image (IOS)
+  if (pwa.manifest.iosSplashSreen) {
+    // get all images name , split `-` ,  get w and h , then inject
+    head.link.push(
+      { href: `${pwa.manifest.iosSplashSreen.targetDir}/iphone6-splash-screen.png`, media: "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)", rel: "apple-touch-startup-image" }
+      // < link href = "splashscreens/iphone5_splash.png" media = "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel = "apple-touch-startup-image" />
+      // <link href="splashscreens/iphone6_splash.png" media = "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel = "apple-touch-startup-image" />
+      // <link href="splashscreens/iphoneplus_splash.png" media = "(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel = "apple-touch-startup-image" />
+      // <link href="splashscreens/iphonex_splash.png" media = "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel = "apple-touch-startup-image" />
+      // <link href="splashscreens/iphonexr_splash.png" media = "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel = "apple-touch-startup-image" />
+      // <link href="splashscreens/iphonexsmax_splash.png" media = "(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" rel = "apple-touch-startup-image" />
+      // <link href="splashscreens/ipad_splash.png" media = "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" rel = "apple-touch-startup-image" />
+      // <link href="splashscreens/ipadpro1_splash.png" media = "(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel = "apple-touch-startup-image" />
+      // <link href="splashscreens/ipadpro3_splash.png" media = "(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel = "apple-touch-startup-image" />
+      // <link href="splashscreens/ipadpro2_splash.png" media = "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel = "apple-touch-startup-image" />
+    )
   }
 
   // Title
