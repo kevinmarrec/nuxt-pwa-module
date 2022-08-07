6153ba458f24faadf3bb473510b0bf6f6fd8add8
HomWang <ajake123123@gmail.com>
2022-08-07 13:27:45 +0800

    modify type image-cache type and add SVGA

--- templates/workbox/sw.js
+++ templates/workbox/sw.js
@@ -35,15 +35,17 @@ registerRoute(
   })
 )
 
-// Cache images with a Cache First strategy
+// Demonstrates a custom cache name for a route.
+// https://github.com/GoogleChrome/workbox/blob/63ea2ca52e7278411a71520f48bd9cf700694edd/demos/src/workbox-sw/sw.js
 registerRoute(
-  ({ request }) => request.destination === 'image',
-  new CacheFirst({
-    cacheName: 'images',
+  new RegExp('.*\\.(?:png|jpg|jpeg|svg|gif|svga)'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'image-cache',
     plugins: [
+      // Ensure that only requests that result in a 200 status are cached
       new CacheableResponsePlugin({ statuses: [200] }),
       // 50 entries max, 30 days max
       new ExpirationPlugin({ maxEntries: 50, maxAgeSeconds: 60 * 60 * 24 * 30 })
-    ]
-  })
-)
+    ],
+  }),
+);
