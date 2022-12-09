8b8f86d2d86fa0b36c10bff778330acf7c9134b0
Bobbie Goede <bobbiegoede@gmail.com>
2022-12-09 10:57:49 +0100

    feat: add runtime config support for swUrl

--- src/runtime/nitro-plugin.ts
+++ src/runtime/nitro-plugin.ts
@@ -4,11 +4,15 @@ import { useRuntimeConfig } from '#imports'
 
 export default <NitroAppPlugin> function (nitro) {
   nitro.hooks.hook('render:html', (htmlContext) => {
+    const config = useRuntimeConfig()
+    const baseUrl = config.baseURL
+    const swUrl = config?.pwa?.workbox?.swUrl ?? 'sw.js'
+
     htmlContext.head.push(
       [
         '<script>',
         'if (\'serviceWorker\' in navigator) {',
-        `  window.addEventListener('load', () => navigator.serviceWorker.register('${joinURL(useRuntimeConfig().app.baseURL, 'sw.js')}'))`,
+        `  window.addEventListener('load', () => navigator.serviceWorker.register('${joinURL(baseUrl, swUrl)}'))`,
         '}',
         '</script>',
       ].join('\n'),
